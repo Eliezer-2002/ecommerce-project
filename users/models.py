8e820dd6b82cebffa75c8b222cada969d0e5d1a4
Eliezer-2002 <eliezerfsdev@gmail.com>
2026-05-21 17:54:02 +0530

    Step 2: Django project setup, 4 apps, custom user model, PostgreSQL config

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+    is_seller = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
